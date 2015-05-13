bc9b025a162e3d5f700319b057498d3bfcd539ce
dub <vennde@yandex.ru>
2015-05-13 16:09:30 +0300

    layers init

--- lib/gosu_tiled/map.rb
+++ lib/gosu_tiled/map.rb
@@ -10,18 +10,29 @@ module Gosu
         @width = data['width'] * data['tilewidth']
         @height = data['height'] * data['tileheight']
         @tilesets = Tilesets.new(window, data['tilesets'], data_dir)
-        @layers = Layers.new(window,
-                             data['layers'],
-                             width: @width,
-                             height: @height,
-                             tile_width: data['tilewidth'],
-                             tile_height: data['tileheight'])
+        @layers = layers_init(data)
       end
 
       def draw(offset_x, offset_y)
         @layers.each do |layer|
           layer.draw(offset_x, offset_y, tilesets)
         end
+      end
+
+      def layers_init(data)
+        entity = 
+          if data['orientation'] == 'isometric'
+            Isometric
+          else
+            Layers
+          end
+
+        entity.new(window,
+                   data['layers'],
+                   width: @width,
+                   height: @height,
+                   tile_width: data['tilewidth'],
+                   tile_height: data['tileheight'])
       end
     end
   end
